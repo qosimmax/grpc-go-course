fb7a6dc457aaf4e7ef0ea33d278784e92875764e
kasimkhudaybergenov <qosimmax@gmail.com>
2019-06-05 21:59:15 +0500

    calculator unary rpc

---
+++ calculator/calculator_client/client.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"log"
+
+	"qosimmax/grpc-go-course/calculator/calculatorpb"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	fmt.Println("Calculator Client")
+	cc, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatalf("could not connect: %v", err)
+	}
+
+	defer cc.Close()
+
+	c := calculatorpb.NewCalculatorServiceClient(cc)
+
+	doUnary(c)
+
+}
+
+func doUnary(c calculatorpb.CalculatorServiceClient) {
+	fmt.Println("Starting to do a Unary Sum RPC ...")
+	req := &calculatorpb.SumRequest{
+		FirstNumber:  5,
+		SecondNumber: 10,
+	}
+
+	res, err := c.Sum(context.Background(), req)
+	if err != nil {
+		log.Fatalf("error while calling Greet RPC: %v", err)
+	}
+
+	log.Printf("Response from Sum: %v", res.SumResult)
+}
---
+++ calculator/calculator_server/server.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	"qosimmax/grpc-go-course/calculator/calculatorpb"
+
+	"google.golang.org/grpc"
+)
+
+type server struct{}
+
+func (server) Sum(ctx context.Context, req *calculatorpb.SumRequest) (*calculatorpb.SumrResponse, error) {
+	fmt.Printf("Received Sum RPC: %v\n", req)
+	firstNumber := req.FirstNumber
+	secondNumber := req.SecondNumber
+
+	sum := firstNumber + secondNumber
+
+	res := &calculatorpb.SumrResponse{
+		SumResult: sum,
+	}
+
+	return res, nil
+}
+
+func main() {
+	fmt.Println("Calculator Server")
+
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+
+	calculatorpb.RegisterCalculatorServiceServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+
+}
